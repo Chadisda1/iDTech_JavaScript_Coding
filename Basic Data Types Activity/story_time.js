917ea484db1e7520eb25434af85740ea6f8591e6
Chadisda1 <chadisda1@gmail.com>
2022-07-21 13:34:47 -0500

    git push

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,31 @@
+// TODO: Create 4-String variables to introduce develop your story.
+var name = "Chase"
+var introduction = "Hi! How nice to see you!"
+var order = "Here are the things I want to order:"
+var drink = "Sprite"
+
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+var age = 23
+var numberOfDrinks = 4
+var time = 9
+
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+var food = ["chicken", "beef", "noodles", "ham"]
+
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+var foodGood = true
+
+
+// TODO: Print your story to the console.
+console.log(name + " had just turned " + age + ".");
+console.log("He walked into his favorite restaurant to celebrate.")
+console.log(name + ' greeted his favorite waiter, "' + introduction + '."')
+console.log("The waiter asked him what he wanted to eat.")
+console.log(name + ' responded, "' + order + '..."')
+console.log(name + " had " + numberOfDrinks + " glasses of " + drink + ".")
+console.log("Although he enjoyed his meal, he was most delighted with the " + food[2] + " and " + food[0] + ".")
+console.log("When his friend told him that the food must have been delicious, " + name + " said " + foodGood + ", it was very good.")
+console.log(name + " finally left the restaruant satisfied at " + time + " O'clock.")
